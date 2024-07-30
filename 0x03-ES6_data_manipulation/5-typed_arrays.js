1e07dd88a47a37c5abc95c69d0ec4ea3c659fb8f
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-07-30 19:56:21 +0300

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const dataview = new DataView(buffer, 0, length);
+  dataview.setInt8(position, value);
+  return dataview;
+}
