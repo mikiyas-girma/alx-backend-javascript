b66b1ad44e96caa0bd80a29a0c16ecb048abe53d
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-07-30 22:40:41 +0300

    Map updation

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  items.forEach((value, key) => {
+    if (value === 1) {
+      items.set(key, 100);
+    }
+  });
+}
