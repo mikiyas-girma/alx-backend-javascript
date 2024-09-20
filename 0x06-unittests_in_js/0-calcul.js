4c141d00bedb59b2b3c5175766cd2135ceac4d80
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-20 21:44:51 +0300

    0x06 unittests in js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+function calculateNumber(a, b) {
+  return a + b;
+}
+
+
+export { calculateNumber };
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+import assert from 'assert';
+import { calculateNumber } from './0-calcul.js';
+
+describe('calculateNumber', function() {
+    it('should return the sum of two integers', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+    it('should return the sum of two floats', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.2), 4.7);
+    });
+    it('should return the sum of two floats rounded', function() {
+        assert.strictEqual(calculateNumber(1.4, 4.5), 5.9);
+    });
+    });
