feb6dc3172b869ae565af59c3c48acd0d11f7b7a
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-21 00:10:19 +0300

    using stubs for testing

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,18 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const expect = chai.expect;
+const utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
+
+describe("sendPaymentRequestToApi", function () {
+  it("should log the correct total", function () {
+    const consoleSpy = sinon.spy(console);
+    const calculateNumberStub = sinon
+      .stub(utils, "calculateNumber")
+      .returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWithExactly("SUM", 100, 20)).to.be
+      .true;
+    expect(consoleSpy.log.calledOnceWithExactly("The total is: 10")).to.be.true;
+  });
+});
