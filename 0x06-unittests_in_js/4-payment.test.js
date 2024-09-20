11a3bfd598577b326e01fa05ecda51a2ea6e1571
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-21 00:22:15 +0300

    my final test suite modification

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,13 +6,14 @@ const sendPaymentRequestToApi = require("./4-payment");
 describe("sendPaymentRequestToApi", function () {
   it("should log the correct total", function () {
     const consoleSpy = sinon.spy(console);
-    const calculateNumberStub = sinon
-      .stub(Utils, "calculateNumber")
-      .returns(10);
+    const calculateNumberStub = sinon.stub(Utils, "calculateNumber");
+    calculateNumberStub.returns(10);
+
     sendPaymentRequestToApi(100, 20);
     expect(calculateNumberStub.calledWith("SUM", 100, 20)).to.be.true;
     expect(calculateNumberStub.callCount).to.be.equal(1);
     expect(consoleSpy.log.calledWith("The total is: 10")).to.be.true;
+    expect(consoleSpy.log.callCount).to.be.equal(1);
     calculateNumberStub.restore();
     consoleSpy.log.restore();
   });
