c387e94a80a355300cebf153356f85180addf579
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-20 22:07:06 +0300

    changed the function to return rounded answer with some tests added

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,9 +6,15 @@ describe("calculateNumber", function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it("should return the sum of two floats", function () {
-    assert.strictEqual(calculateNumber(1.5, 3.2), 4.7);
+    assert.strictEqual(calculateNumber(1.5, 3.2), 5);
   });
   it("should return the sum of two floats rounded", function () {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 5.9);
+    assert.strictEqual(calculateNumber(1.4, 4.5), 6);
+  });
+  it("should return the sum of two floats rounded", function () {
+    assert.strictEqual(calculateNumber(0.0, 0), 0);
+  });
+  it("should return the sum of two floats rounded", function () {
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
   });
 });
